812150a0444812af5602b00693a9a7d7329f2ce2
Timi okewunmi <okewunmitimileyin23@gmail.com>
2025-10-06 10:38:52 +0100

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVu5IQ3Y5bvgAfSKym8kXPpZZWKqMELTM",
+  authDomain: "sublime-logs.firebaseapp.com",
+  projectId: "sublime-logs",
+  storageBucket: "sublime-logs.firebasestorage.app",
+  messagingSenderId: "381847722551",
+  appId: "1:381847722551:web:8ec68f018291d3927f3db2",
+  measurementId: "G-8YHHJC1MP7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+console.log("✅ Firebase initialized successfully");
